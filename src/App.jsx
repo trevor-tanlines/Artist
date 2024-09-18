4a8835d923fef1a723527f50940f6c786c1c618b
Trevor <ttan20@my.bcit.ca>
2024-09-18 00:34:42 -0500

    Finished Assignment

--- src/App.jsx
+++ src/App.jsx
@@ -4,121 +4,57 @@ function App() {
         <section id="content">
           <header>
             <div>
-                <h1>Shop Mart</h1>
-                <div>
-                  <button className="icon-btn">
-                    <span>&#129293;</span>
-                    </button>
-
-                  <span className="badge">3</span>
-                  
-                  <button className="icon-btn">
-                  <span>&#128722;</span>
-                  <span className="badge">1</span>
-                  </button>
-                </div>
+                <h1>Vundabar</h1>
             </div>
 
-            <div>
-              <nav>
-                <button>Home</button>
-                <button>Catalog</button>
-                <button>All Products</button>
-                <button>Wishlist</button>
-              </nav>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/e/e0/Vundabar_-_Less_Playboy_is_More_Cowboy_5%2C_Le_Confort_Moderne%2C_Poitiers_%282014-05-29_21.50.07_by_Xi_WEG%29.jpg"></img>
 
-              <form>
-                <input type="search" placeholder="search" />
-                <button type="button">Go</button>
-              </form>
+            <div className="Vundabar">
+              <div className="Center">
+                <nav>
+                <button>Vundabar's Albums</button>
+                </nav>
+              </div>
             </div>
           </header>
 
-          <main>
-            <div className="Product">
-            <img src=""></img>
-            <p className="pr-name">Shoes</p>
-            <p className="pr-price">$40</p>
-            </div>
-          </main>
+          <details>
+            <summary>Gawk</summary>
+              <ol>
+                <p>1. Chop</p>
+                <p>2. Oulala</p>
+                <p>3. Cotton Kid</p>
+                <p>4. Darla</p>
+                <p>5. Alien Blues</p>
+                <p>6. Ash In The Sun</p>
+                <p>7. Bust</p>
+                <p>8. Worn/Wander</p>
+                <p>9. Desert Diddy</p>
+                <p>10. Smile Boyo</p>
+              </ol>
+          </details>
 
-          <main>
-            <div className="Product">
-            <img src=""></img>
-            <p className="pr-name">Shoes</p>
-            <p className="pr-price">$40</p>
-            </div>
-          </main>
 
-          <main>
-            <div className="Product">
-            <img src=""></img>
-            <p className="pr-name">Shoes</p>
-            <p className="pr-price">$40</p>
-            </div>
-          </main>
-
-          <main>
-            <div className="Product">
-            <img src=""></img>
-            <p className="pr-name">Shoes</p>
-            <p className="pr-price">$40</p>
-            </div>
-          </main>
-
-          <main>
-            <div className="Product">
-            <img src=""></img>
-            <p className="pr-name">Shoes</p>
-            <p className="pr-price">$40</p>
-            </div>
-          </main>
-
-          <main>
-            <div className="Product">
-            <img src=""></img>
-            <p className="pr-name">Shoes</p>
-            <p className="pr-price">$40</p>
-            </div>
-          </main>
-
-          <main>
-            <div className="Product">
-            <img src=""></img>
-            <p className="pr-name">Shoes</p>
-            <p className="pr-price">$40</p>
-            </div>
-          </main>
+          <details>
+            <summary>Smell Smoke</summary>
+              <ol>
+                <p>1. Acetone</p>
+                <p>2. Harvest</p>
+                <p>3. Tar Tongue</p>
+                <p>4. Tonight I'm Wearing Silk</p>
+                <p>5. No People To Person</p>
+                <p>6. Big Funny</p>
+                <p>7. Diver</p>
+                <p>8. $$$</p>
+                <p>9. Hold a Light</p>
+                <p>10. A Man Loses a Hat</p>
+              </ol>
+          </details>
 
         </section>
         <footer>
 
-        <div>
-            <h3>Our Products</h3>
-            <ul>
-              <li><a href="">Home Page</a></li>
-              <li><a href="">Search</a></li>
-              <li><a href="">Catalog</a></li>
-            </ul>
-          </div>
-
-        <div>
-            <h3>Our Policies</h3>
-            <ul>
-              <li><a href="">Return Policy</a></li>
-              <li><a href="">Shipping Policy</a></li>
-              <li><a href="">Terms of Service</a></li>
-            </ul>
-          </div>
-
-          <div>
-            <h3>Our Store</h3>
-            <ul>
-              <li><a href="">About Us</a></li>
-              <li><a href="">Contact Us</a></li>
-              <li><a href="">Sell With Us</a></li>
-            </ul>
-          </div>
+        
         </footer>
       </div>
   );
